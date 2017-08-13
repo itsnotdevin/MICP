eaa13fe71fab8725e17d9d5152e375d1ff6ac665
itsnotdevin <itsnotdevin@gmail.com>
2017-08-13 14:04:14 -0400

    MICP Week 4
    TEBOW IT: 49 minutes.  Tried to improve from last week by creating more examples, a better walkthrough and more test cases.  
    I recognize I made a mistake by completing the T before the W portion as soon as I started the implementation.  I did not want to waste time going back, so I just scratched out the name and completed it last.

---
+++ week4.java
@@ -0,0 +1,57 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class week4 {
+    public static boolean findString(String dict, String word) throws IllegalArgumentException
+    {
+        if (dict == null || word == null) throw new IllegalArgumentException("String or Dictionary were null");
+        if (dict.length() < 1 || word.length() < 1) return false;
+        String[] parsedDict = dict.split(", ");
+        return findString(parsedDict, word);
+    }
+
+    private static boolean findString(String[] dict, String word)
+    {
+        if (word.length() == 0) return true;
+        for (int i = 0; i < dict.length; i++)
+        {
+            if (dict[i].length() > word.length()) continue;
+            if (word.substring(0,dict[i].length()).equalsIgnoreCase(dict[i]))
+                return findString(dict, word.substring(dict[i].length(),word.length()));
+        }
+       return false;
+    }
+
+    @Test
+        public void testFindString() {
+            System.out.println("* testFindString JUnit4Test: test method 1 - assertEquals()");
+            //Test cases for true output: all words found w/ extra dictionary input, repeating words found, different ways to pass,
+            // one letter dictionary inputs, repeating dictionary input, one word dictionary and word
+            assertEquals(true, week4.findString("pear, salmon, you, sun, girl, enjoy", "youenjoy"));
+            assertEquals(true, week4.findString("pear, salmon, you, sun", "youyouyouyou"));
+            assertEquals(true, week4.findString("pear, salmon, pearsalmon", "pearsalmon"));
+            assertEquals(true, week4.findString("a, b, c, d, e, f, g", "aaaaaaabcdefgaa"));
+            assertEquals(true, week4.findString("ab, ab, ab, ab", "ababababababab"));
+            assertEquals(true, week4.findString("pear", "pear"));
+
+            //Test cases for false output: no words found, false at end, false at beginning,one word dictionary and word
+            //empty dictionary, empty word, empty word and dictionary
+            assertEquals(false, week4.findString("pear, twice", "bob"));
+            assertEquals(false, week4.findString("pear, bob", "bobsalty"));
+            assertEquals(false, week4.findString("pear, bob", "saltybob"));
+            assertEquals(false, week4.findString("pear", "bob"));
+            assertEquals(false, week4.findString("", "saltybob"));
+            assertEquals(false, week4.findString("pear, bob", ""));
+            assertEquals(false, week4.findString("", ""));
+
+        }
+        @Test (expected=IllegalArgumentException.class)
+        public void checkArgumentException() {
+            System.out.println("* testFindString JUnit4Test: test method 2 - checkArgumentException()");
+            //Test cases for exception: null dictionary, null word, null dictionary and word,
+            week4.findString(null, "word");
+            week4.findString("dictionary, word", null);
+            week4.findString(null, null);
+        }
+
+}
