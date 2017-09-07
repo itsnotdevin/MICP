c0144e7c6a97825b1ef5c9a1028c4094970dd979
itsnotdevin <itsnotdevin@gmail.com>
2017-09-07 16:47:48 -0400

    Updated with corrections

--- week6.java
+++ week6.java
@@ -1,19 +1,4 @@
-package micp;
-
-/* space complexity: hasSubtree() O(tmp1 + tmp2) where tmp1 and tmp2 are copies of S and T
-                     findSubtree() O(hS) where hS is height of subtree S
-                     isEqual() O(hT) where hT is height of subtree T
-              total: isEqual(min(hS,hT)+tmp1+tmp2)
-                     since our recursive calls terminate once of the two trees have reached null, we will only ever
-                     have the smallest tree on our call stack at one time.
-
-   time complexity: hasSubtree() O(1) constant amount of work.
-                    findSubtree() O(2^hS) where hS is the height of subtree S.
-                    isEqual() O(2^hT) where hT is the height of subtree T.
-               total: (O(2^hS)* min(2^hS, 2^hT) + O(1))
-                    findSubtree runs for the entire height of tree S.
-                    isEqual only runs until the smaller of the two trees reach null, potentially every iteration of findSubtree.
-*/
+package solution;
 
 class treeNode{
     treeNode left, right;
@@ -23,14 +8,8 @@ class treeNode{
     }
 }
 public class week6 {
-    public static boolean hasSubtree(treeNode T, treeNode S) throws IllegalArgumentException {
-        if (T == null && S == null) 
-            throw new IllegalArgumentException("hasSubtree(T, S): T and S cannot both be null.");
-        treeNode tmp1 = T; 
-        treeNode tmp2 = S;
-        return findSubtree(tmp1, tmp2);
-    }
-    private static boolean findSubtree(treeNode T, treeNode S){        
+    public static boolean hasSubtree(treeNode T, treeNode S){
+        if (T == null && S == null) throw new IllegalArgumentException("hasSubtree(treeNode T, treeNode S) T and S cannot both be null.");
         if (T == null) return true;
         if (S == null) return false;
         if (T.data == S.data){
@@ -38,7 +17,7 @@ public class week6 {
                 return true;
             }
         }
-        return (findSubtree(T, S.left) || findSubtree(T,S.right));
+        return (hasSubtree(T, S.left) || hasSubtree(T,S.right));
     }
     private static boolean isEqual(treeNode T, treeNode S) {
         if (T == null && S == null) return true;
